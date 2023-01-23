cdeb52ca162cd0187a2576799a9eca04c0de785e
Zachary Montgomery <zachmontgo@iodinedev.com>
2023-01-23 16:30:03 -0600

    Ayo

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,19 @@
 use std::process::Command;
 
 fn main() {
+    test("Starting the script...");
+
     let output = Command::new("bash")
         .arg("-c")
-        .arg("pwd && cp -a ./custom/. ./julia_scrapers/custom && cd julia_scrapers && python3 run.py && cd .. && cp -a ./julia_scrapers/output/. ./julia_website/output && cd julia_website && docker compose up -d")
+        .arg("pwd && mkdir -p ./custom && mkdir -p ./goebel_website/output && rm -rf ./goebel_scraper/custom && mkdir -p ./goebel_scrapers/custom && cp -a ./custom/. ./goebel_scrapers/custom && cd goebel_scrapers && python3 run.py && cd .. && cp -a ./goebel_scrapers/output/. ./goebel_website/output && docker compose up -d")
         .output()
         .expect("failed to execute process");
 
     println!("status: {}", output.status);
     println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
     println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+}
+
+fn test(prt: &str) {
+    println!("{}", prt);
 }
